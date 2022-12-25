5b4f9c290e7f92f342a868cef6871257e360ec6d
Bruno Gomes <gomes.bruno201@gmail.com>
2022-12-25 01:03:34 -0300

    wip: adds new layout to program

--- root/settings.py
+++ root/settings.py
@@ -1,6 +1,10 @@
 flask_config = {
     "static_folder" : "./templates", 
-    "template_folder": "./templates"
+    "template_folder": "./templates",
 }
 
-window_config = {}
+window_config = {
+    "width": 340,
+    "height": 455,
+    "resizable": False,
+}
